b23c18e129abcfac0a2e3abf5b917e1fdb402ef6
Brandon <brandonbgrayson@gmail.com>
2021-06-15 15:48:34 -0400

    added recipe component with angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule

7adbce84016e3eabef2f6c7c8aee3d0c7ba14c07
Brandon <brandonbgrayson@gmail.com>
2021-06-16 01:09:20 -0400

    updated recipe list html component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
- recipeList: Recipe[] = []
+ recipeList: Recipe[] = [
+   // create a new instance of recipe
+   new Recipe("First Recipe", "This is my first recipe!", "https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2020%2F07%2F22%2F8000900-2000.jpg")
+ ]
   constructor() { }
 
   ngOnInit(): void {

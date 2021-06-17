1a55054f1a73b1ed24fd9e7942559413af653543
Brandon <brandonbgrayson@gmail.com>
2021-06-17 18:47:21 -0400

    added ngFor loop inside shopping list to loop over ingredients array

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+    public name: string;
+    public amount: number;
+    
+    constructor(name: string, number: number) {
+        this.name = name
+        this.amount = number
+    }
+}
